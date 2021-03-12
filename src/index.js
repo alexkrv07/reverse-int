842a59c1e1d35e9284e733e8cdfe141cd63b7f28
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-03-12 20:41:25 +0200

    fix: delete return error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function reverse (n) {
-    if (arguments.length > 1 || !Number.isInteger(n)) {
-        return "error";
-      }
-      return parseInt(n.toString().split("").reverse().join(""));
+    return parseInt(n.toString().split("").reverse().join(""));
 }
