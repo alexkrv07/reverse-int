22aa0b1ee58254c544d999246fc1dbeff20d8bb2
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-03-12 20:27:52 +0200

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if (arguments.length > 1 || !Number.isInteger(n)) {
+        return "error";
+      }
+      return parseInt(n.toString().split("").reverse().join(""));
 }
